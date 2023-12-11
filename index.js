974dd93ea0a4dda38c7aa3f781c5906a711a38e0
Clément POIRET <poiret.clement@outlook.fr>
2023-12-11 17:09:21 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+import { Elm } from "./src/Main.elm";
+
+if (process.env.NODE_ENV === "development") {
+    const ElmDebugTransform = await import("elm-debug-transformer")
+
+    ElmDebugTransform.register({
+        simple_mode: true
+    })
+}
+
+Elm.Main.init({
+    node: document.getElementById("root")
+});
